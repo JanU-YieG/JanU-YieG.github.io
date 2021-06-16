f6ea9d5818795a060049258a3406fc65604683b9
borer <janu.yieg.borer@gmail.com>
2021-06-16 17:24:55 +0800

    rebuilding site Wed Jun 16 05:24:55 PM CST 2021

---
+++ docs/my_github_repository_links/index.md
@@ -0,0 +1,6 @@
+# My-github-repository-links
+
+![UDqYdWk-arch-linux-wallpaper.jpg](https://img.maocdn.cn/img/2021/06/07/UDqYdWk-arch-linux-wallpaper.jpg)
+## my linux profiles
+[dotfiles](https://github.com/JanU-YieG/dotfiles)
+
--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# hugo -d ./docs
+hugo -d ./docs
 current_branch=$(git branch --show-current)
 # Add changes to git.
 git pull --rebase --autostash origin $current_branch
@@ -17,26 +17,26 @@ git push origin $current_branch
 
 
 # If a command fails then the deploy stops set -e 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 # hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-hugo -t LoveIt
+# hugo -t LoveIt
 
 # Go To Public folder
-cd public
+# cd public
 
-current_branch=$(git branch --show-current)
-echo "$current_branch"
+# current_branch=$(git branch --show-current)
+# echo "$current_branch"
 # Add changes to git.
-git add .
+# git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+# msg="rebuilding site $(date)"
+# if [ -n "$*" ]; then
+	# msg="$*"
+# fi
+# git commit -m "$msg"
 
 # Push source and build repos.
-git push origin $current_branch
+# git push origin $current_branch
