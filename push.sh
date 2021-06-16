fbd13936abbb0930a7c5d8de5f0af21b41af8517
borer <janu.yieg.borer@gmail.com>
2021-06-16 18:46:52 +0800

    rebuilding site Wed Jun 16 06:46:52 PM CST 2021

--- push.sh
+++ push.sh
@@ -17,42 +17,42 @@ git push origin $current_branch
 
 
 
-git checkout site-code
-rm -rf *
-git checkout main -- public
-cp -Rf public/* ./
-rm -r public
+# git checkout site-code
+# rm -rf *
+# git checkout main -- public
+# cp -Rf public/* ./
+# rm -r public
+# git add .
+# msg="rebuilding site $(date)"
+# if [ -n "$*" ]; then
+    # msg="$*"
+# fi
+# git commit -m "$msg"
+# git push origin site-code
+# git checkout main
+
+
+# If a command fails then the deploy stops set -e 
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t LoveIt
+
+# Go To Public folder
+cd public
+
+# current_branch=$(git branch --show-current)
+# echo "$current_branch"
+# Add changes to git.
 git add .
+
+# Commit changes.
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
     msg="$*"
 fi
 git commit -m "$msg"
-git push origin site-code
-git checkout main
-
-
-# If a command fails then the deploy stops set -e 
-# printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
-# Build the project.
-# hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-# hugo -t LoveIt
-
-# Go To Public folder
-# cd public
-
-# current_branch=$(git branch --show-current)
-# echo "$current_branch"
-# Add changes to git.
-# git add .
-
-# Commit changes.
-# msg="rebuilding site $(date)"
-# if [ -n "$*" ]; then
-	# msg="$*"
-# fi
-# git commit -m "$msg"
 
 # Push source and build repos.
-# git push origin $current_branch
+git push origin $current_branch
